8bd4e08f2e6d8ed5d33fefc441f140d92a456527
whiHak <betse1111111111@gmail.com>
2023-10-25 11:42:48 +0300

    added Navbar comp

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+import logo from "../assets/images/Logo.png";
+
+const Navbar = () => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        display: "flex",
+        justifyContent: "none",
+        mt: { sm: "32px", xs: "20px" },
+        gap: { sm: "122px", xs: "40px " },
+        px: "10px",
+      }}
+    >
+      <Link>
+        <img src={logo} alt="Logo" style={{ width: "48px", height: "48px" }} />
+      </Link>
+      <Stack direction="row" gap="40px" fontSize="24px" sx={{ width: {} }}>
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+            borderBottom: "3px solid #FF2625",
+            height: "max-content",
+            alignItems:"flex-end"
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercise"
+          style={{ textDecoration: "none", color: "#3A1212" }}
+        >
+          Exercise
+        </a>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Navbar;
