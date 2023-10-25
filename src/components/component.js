f0d866df86f8cbd589e5960f51047c4fa8b43e70
whiHak <betse1111111111@gmail.com>
2023-10-25 11:42:06 +0300

    added components

---
+++ src/components/component.js
@@ -0,0 +1,4 @@
+export {default as Home}  from "./Home"
+export {default as Navbar} from "./Navbar"
+export {default as ExerciseDetail} from "./ExerciseDetails"
+export {default as Footer} from "./Footer"
