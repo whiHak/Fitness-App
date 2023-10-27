066b0dc25bb50c5089a0bc2d1b56e00a9b65dd75
whiHak <betse1111111111@gmail.com>
2023-10-27 10:41:08 +0300

    added api configuration

---
+++ src/utils/fetchData.js
@@ -0,0 +1,13 @@
+export const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+  return data;
+};
