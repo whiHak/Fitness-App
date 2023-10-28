aa640076fe54b5c43c75dc0aa18f3a0a16fb22d8
whiHak <betse1111111111@gmail.com>
2023-10-28 13:58:31 +0300

    modified styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Box } from '@mui/material'
 import { ExerciseDetail, Navbar, Home, Footer } from './components/component'
 const App = () => {
   return (
-    <Box  sx={{width:{lg:"1488px", md:"100vw"}, bgcolor:"#FFEEEE", alignItems:"center"}} m="auto">
+    <Box  sx={{width:{lg:"1550px", md:"100vw"}, bgcolor:"#FFEEEE", alignItems:"center"}} m="auto">
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
